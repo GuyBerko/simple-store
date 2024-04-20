ff912c8e2f8fbd2d45ae036b5af12eb52c2230fd
Guy Berkovich <mailofguy@gmail.com>
2024-04-20 12:42:40 +0300

    rename

---
+++ client/src/components/ItemCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import classnames from 'classnames';
+import './ItemCard.css';
+
+const ItemCard = ({ title, limit, itemsRemains, onClick }) => {
+  return (
+    <Grid item xs={6}>
+      <Card onClick={itemsRemains === 0 ? () => {} : onClick}>
+        <CardContent className={classnames({ 'card-content': true, disabled: itemsRemains === 0 })}>
+          <Typography variant='h3' gutterBottom>
+            {title}
+          </Typography>
+          <Typography variant='h4' component='div'>
+            {itemsRemains} / {limit}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export default ItemCard;
